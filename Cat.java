6c1efead6f4743965d1f8ebd03f65eb05e0a3027
ZuluSkyGate <124462623+ZuluSkyGate@users.noreply.github.com>
2024-07-01 21:08:46 +0300

    Commit

--- Cat.java
+++ Cat.java
@@ -1,5 +1,11 @@
 package seminar_2_OOP;
 
 public class Cat extends Animal {
+
+    @Override
+    public void speak() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'speak'");
+    }
     
 }
